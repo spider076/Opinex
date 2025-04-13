178084ddaa4a506c494021b55b42e85a71589652
spider076 <saadabban76@gmail.com>
2025-04-13 09:13:48 +0000

    optimized for one question at a time on hourly basis

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,31 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require("express-cors");
+const cron = require("node-cron");
+const { getTrendingTopics } = require("./services/geminiService");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(cors());
+let latestTopic = null;
 
-app.get("/", (req,res)=> {
-    console.log("hi");
-    return res.send("Hiii");
-})
+// Fetch on startup
+const fetchAndStoreTopics = async () => {
+  const result = await getTrendingTopics();
+  console.log("result ; ", result);
+  latestTopic = result[0] || [];
+  console.log("Updated trending topics at", new Date().toLocaleString());
+};
 
+fetchAndStoreTopics();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+// Schedule to run every hour
+cron.schedule("0 * * * *", fetchAndStoreTopics);
+
+// API endpoint to get trending topics
+app.get("/trending", (req, res) => {
+  res.json(latestTopic);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
